351a28d83eb3318124dee364575e8393326068aa
Falbeh <falbe444@live.dk>
2022-10-04 12:30:04 +0200

    clean up experiments

--- Assignment2/experiments.py
+++ Assignment2/experiments.py
@@ -7,46 +7,35 @@ import numpy as np # type: ignore
 
 TIMEOUT = 30
 
-# run the given jar package ,
-# provide the given arg as the command - line
-# argument,
-# feed the given input string to the stdin of the
-# process,
-# and return the stdout from the process as string
-def run_java (jar: str, arg : str, input : str ) -> str:
-    p = subprocess.Popen (['java','-jar', jar ,arg],
+# run the given jar package
+
+def run_java (jar: str) -> str:
+    p = subprocess.Popen (['java','-jar', jar],
     stdin = subprocess.PIPE,
     stdout = subprocess.PIPE)
-    ( output ,_) = p.communicate(input.encode('utf-8'),
-    timeout = TIMEOUT)
+    ( output ,_) = p.communicate(timeout = TIMEOUT)
     return output.decode ('utf-8')
-
-
-""" if __name__ == '__main__':
-    print (run_java ('app/build/libs/app.jar',
-    'hash ','1000000')) """
-
 
 if __name__ == '__main__':
     with open('results.csv', 'w') as f:
-        writer = csv.DictWriter(f,
+        """ writer = csv.DictWriter(f,
             fieldnames=['n'])
-        writer.writeheader()
-        input: str = run_java('app/build/libs/app.jar','hash','1000000')
+        writer.writeheader() """
+        input: str = run_java('app/build/libs/app.jar')
         results = input.splitlines()
 
         resultsInt = []
         for (n) in results:
             resultsInt.append(int(n))
-            writer.writerow({
+            """ writer.writerow({
                 'n': n
-        }) 
+            })  """
 
-        ## GENERATE PLOT ## 
-    
+        ## GENERATE HISTOGRAM PLOT WITH RESULTS ## 
         plt.hist(resultsInt, density=False, bins=24)  # density=False makes count
         
         plt.ylabel('Count')
-        plt.xlabel('Numbers')
+        plt.xlabel('p(h(x))')
 
+        ## SHOW PLOT
         plt.show()

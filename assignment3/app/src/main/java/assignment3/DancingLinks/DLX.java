a1d2ca8d4965bbd9cba70e07433ed72efd2680f2
Falbeh <falbe444@live.dk>
2022-11-05 15:24:22 +0100

    DLX works

--- assignment3/app/src/main/java/assignment3/DancingLinks/DLX.java
+++ assignment3/app/src/main/java/assignment3/DancingLinks/DLX.java
@@ -1,5 +1,62 @@
 package assignment3.DancingLinks;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DLX {
-    
+
+    // Parameters are ColumnNode (root) and the stack S
+    public static void search(ColumnNode h, List<Node> S) {
+        // If root node is alone in columns then finish
+        if (h.right == h) {
+            ProcedureDLX.print(S); // Printing
+            // Need to terminate here
+        }
+        else {
+            ColumnNode c = ProcedureDLX.choose(h);
+            
+            ProcedureDLX.cover(c);
+            // Iterate over rows that have a one on column c
+            for (Node r = c.down; r != c; r=r.down) {
+                S.add(r);
+                // Cover the columns covered by row r
+                for (Node j = r.right; j != r; j=j.right) {
+                    ProcedureDLX.cover(j.column);
+                }
+                // Recursive call
+                search(h, S);
+
+                // Going back 
+                S.remove(S.size()-1);
+                c = r.column;
+
+                for (Node j = r.left; j != r; j=j.left) {
+                    ProcedureDLX.unCover(j.column);
+                }
+            }
+            ProcedureDLX.unCover(c);
+        }
+    }
+
+    public static void main(String[] args) {
+        // Storing the answer S
+        List<Node> S = new ArrayList<>();
+
+        // Call Sudoku to exact cover
+
+        // Call DancingLinksMatrix
+        ColumnNode h = DancingLinksMatrix.createDancingLinks(new int[][] {
+           /*  {1,0,1},
+            {1,1,0},
+            {0,1,0} */
+            {0, 0, 1, 0, 1, 1, 0},
+            {1, 0, 0, 1, 0, 0, 1},
+            {0, 1, 1, 0, 0, 1, 0},
+            {1, 0, 0, 1, 0, 0, 0},
+            {0, 1, 0, 0, 0, 0, 1},
+            {0, 0, 0, 1, 1, 0, 1}
+        });
+
+        search(h, S);
+    }
 }
--- assignment3/app/src/main/java/assignment3/DancingLinks/ProcedureDLX.java
+++ assignment3/app/src/main/java/assignment3/DancingLinks/ProcedureDLX.java
@@ -1,6 +1,9 @@
 package assignment3.DancingLinks;
 
-public class auxiliaryProceduresDLX {
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProcedureDLX {
 
     // Choosing the column with smallest size 
     public static ColumnNode choose(ColumnNode h) {
@@ -8,18 +11,19 @@ public class auxiliaryProceduresDLX {
         int s = -1;
         ColumnNode c = h;
         for (ColumnNode j = (ColumnNode)h.right; j != h; j=(ColumnNode)j.right) {
-            if (j.size < s || j.size == -1) {
+            if (j.size < s || s == -1) {
                 c = j;
                 s = j.size;
             }
         }
+       
         return c;
     }
 
     public static void cover(ColumnNode cn) {
         // Unlink column header from the list of column headers
         cn.unlinkLeftRight();
-
+        
         // Iterate over rows and cols and cover
         for (Node i = cn.down; i != cn; i=i.down) {
             for (Node j = i.right; j != i; j=j.right) {
@@ -41,7 +45,25 @@ public class auxiliaryProceduresDLX {
         cn.linkLeftRight();
     }
 
-    public static void print() {
-        
+    public static void print(List<Node> S) {
+
+        // List<List<Node>> result = new ArrayList<>();
+        List<Node> Sresult = new ArrayList<>();
+        for (int i = 0; i < S.size(); i++) {
+            Sresult.add(S.get(i));
+        }
+
+        // Removing the element at the last index of the list
+        while (!Sresult.isEmpty()) {
+            // System.out.println("hm");
+            Node n = Sresult.remove(Sresult.size()-1); 
+            Node j = n;
+            do {
+                System.out.print(j.column.name);
+                j = j.right;
+            }while(j!=n);
+            System.out.println();
+        }
+        // return result;
     }
 }
